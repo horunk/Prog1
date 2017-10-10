a3724cbc07c0a88e89dfe527fb74044cfa794536
Unknown <horunk@ttu.ee>
2017-10-10 16:38:38 +0300

    test

--- src/ee/itcollege/ics0004/Main.java
+++ src/ee/itcollege/ics0004/Main.java
@@ -3,7 +3,7 @@ package ee.itcollege.ics0004;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("pumun");
+        System.out.print("Ellou");
 	// write your code here
     }
 }
