5dc2261e7fe6128e6beea41fc86b532a886c7e8f
Unknown <horunk@ttu.ee>
2017-11-01 15:30:15 +0200

    Class 01.11.2017, Ex5 added

---
+++ src/Practice5/Exercise5.java
@@ -0,0 +1,20 @@
+package Practice5;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        //ADD METHOD CALL AND OUTPUT
+        System.out.println(random(1,77));
+        System.out.println(random(1,77));
+        System.out.println(random(1,77));
+        System.out.println(random(1,77));
+        System.out.println(random(1,77));
+        
+    }
+
+    public static int random(int min, int max){
+        int range = max - min + 1;
+            return (int)(Math.random() * range)+min;
+
+    }
+}
